1b22aa5f224d31ded9a6143044e27fd44cf2afb8
JasonLim1009 <107684179+JasonLim1009@users.noreply.github.com>
2022-10-24 12:33:16 +0400

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,63 +14,50 @@ const staggerContainer = {
 };
 
 const Footer = () => {
-  
+
   const { about, links, program, newsletter } = footerData;
   return (
     <footer className='bg-dark section'>
       <div className='container mx-auto'>
-        {/* grid */}
         <motion.div
           variants={staggerContainer}
           initial={'hidden'}
           whileInView={'show'}
           viewport={{ once: false, amount: 0.1 }}
-          className='flex flex-col lg:flex-row lg:justify-between text-white gap-x-5 gap-y-14'
-        >
-          {/* about */}
+          className='flex flex-col lg:flex-row lg:justify-between text-white gap-x-5 gap-y-14'>
           <motion.div
             variants={fadeIn('up')}
-            className='flex-1 flex flex-col gap-y-6'
-          >
-            {/* title */}
+            className='flex-1 flex flex-col gap-y-6'>
             <div className='font-primary text-xl uppercase tracking-[0.08em]'>
               {about.title}
             </div>
-            {/* subtitle */}
             <div className='leading-relaxed text-[#DBDBDB]'>
               {about.subtitle}
             </div>
-            {/* address & phone & email */}
             <div className='flex flex-col gap-y-4 font-semibold text-[#DBDBDB]'>
-              {/* address */}
               <div className='flex items-center gap-x-[10px]'>
                 <div>{about.address.icon}</div>
                 <div>{about.address.name}</div>
               </div>
-              {/* phone */}
               <div className='flex items-center gap-x-[10px]'>
                 <div>{about.phone.icon}</div>
                 <div>{about.phone.number}</div>
               </div>
-              {/* email */}
               <div className='flex items-center gap-x-[10px]'>
                 <div>{about.email.icon}</div>
                 <div>{about.email.address}</div>
               </div>
             </div>
           </motion.div>
-          {/* links */}
           <motion.div
             variants={fadeIn('up')}
-            className='flex-1 flex flex-col xl:items-center'
-          >
+            className='flex-1 flex flex-col xl:items-center'>
             <div>
               <div className='font-primary text-xl uppercase tracking-[0.08em] mb-6'>
                 {links.title}
               </div>
               <ul className='flex flex-col gap-y-4 text-[#DBDBDB]'>
                 {links.items.map((item, index) => {
-                  // destructure item
                   const { href, name } = item;
                   return (
                     <li key={index}>
@@ -83,7 +70,6 @@ const Footer = () => {
               </ul>
             </div>
           </motion.div>
-          {/* program */}
           <motion.div variants={fadeIn('up')} className='flex-1'>
             <div className='font-primary text-xl uppercase tracking-[0.08em] mb-6'>
               {program.title}
@@ -94,7 +80,6 @@ const Footer = () => {
               })}
             </ul>
           </motion.div>
-          {/* newsletter */}
           <motion.div variants={fadeIn('up')} className='flex-1'>
             <div className='font-primary text-xl uppercase tracking-[0.08em] mb-6'>
               {newsletter.title}
